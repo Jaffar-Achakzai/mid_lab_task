7398f621b4d631e7f74cead1cf770d1cae89ab8e
Jaffar-Achakzai <jaffar.bscsf21@iba-suk.edu.pk>
2025-03-24 22:14:47 -0700

    Added css and Html code

--- src/component/CountDownLightSwitch.jsx
+++ src/component/CountDownLightSwitch.jsx
@@ -2,32 +2,31 @@ import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
-
-
-
-
 function CountDownLightSwitch() {
-   
-    const formik = useFormik({
-      
-      });
+  const formik = useFormik({});
 
-    return (
-<>    <div className="MId LAb TASk ">
-      <h2>CountDown And Light Switch</h2>
-      <form onstarttimer={formik.handlestarttimer}>
-      
-      
-        {/* starttimer Button */}
-        <br/><br/><br/>
-        <button onclick={"on"}>click</button><br/>Light mode<br/>
-       
-        {/* starttimer Button */}
-        <br/>
-        <button type="starttimer">Start Timer</button>
-      </form>
-    </div>
-</>
-   );
+  return (
+    <>
+      <div className="MId LAb TASk ">
+        <h2>CountDown And Light Switch</h2>
+        <form onstarttimer={formik.handlestarttimer}>
+          {/* starttimer Button */}
+          <br />
+          <br />
+          <br />
+          <button onclick={"on"}>click</button>
+          <br />
+          Light mode
+          <br />
+          <br />
+
+          {/*Timer*/ }
+          <h2>30s</h2>
+          {/* starttimer Button */}
+          <button type="starttimer">Start Timer</button>
+        </form>
+      </div>
+    </>
+  );
 }
 export default CountDownLightSwitch;
